13f8df967578c63bc3bc8725fcfd025b5d26093d
Jorge Olarte <ingeniero@jorgeolarte.com>
2022-03-08 10:36:24 -0500

    fix tilde

--- pages/academy/index.js
+++ pages/academy/index.js
@@ -2,7 +2,7 @@ export default function index() {
   return (
     <div className='flex justify-center items-center mx-auto h-screen'>
       <div>
-        <h1 className='text-7xl animate-pulse'>En construccíon...</h1>
+        <h1 className='text-7xl animate-pulse'>En construcción...</h1>
       </div>
     </div>
   );
