81e31c7d46de67eba08cf046cc6689ca133c4100
Jorge Olarte <ingeniero@jorgeolarte.com>
2022-03-08 10:35:07 -0500

    Academy stats

---
+++ pages/academy/index.js
@@ -0,0 +1,9 @@
+export default function index() {
+  return (
+    <div className='flex justify-center items-center mx-auto h-screen'>
+      <div>
+        <h1 className='text-7xl animate-pulse'>En construccíon...</h1>
+      </div>
+    </div>
+  );
+}
