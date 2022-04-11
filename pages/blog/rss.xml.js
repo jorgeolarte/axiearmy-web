2587937379e3fe126753b0434c58c061e7fd764e
Jorge Olarte <ingeniero@jorgeolarte.com>
2022-04-11 13:56:48 -0500

    Using atom link on rss

--- pages/blog/rss.xml.js
+++ pages/blog/rss.xml.js
@@ -14,10 +14,10 @@ export const getServerSideProps = async ({ res }) => {
   });
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8" ?>
-    <rss version="2.0">
+    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
         <title>Axie Army | Academy</title>
-        <link>https://axiearmy.club</link>
+        <link>https://axiearmy.club/</link>
         <atom:link href="https://axiearmy.club/blog/rss.xml" rel="self" type="application/rss+xml" />
         <description>Invertimos en videojuegos blockchain. Damos becas a jugadores que quieren hacer realidad sus sueños. Porque jugar es un estilo de vida. #GamingIsALifeStyle</description>
         <copyright>${new Date().getFullYear()} Refsnes Data as. Todos los derechos reservados.</copyright>
