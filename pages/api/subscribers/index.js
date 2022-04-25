9aebb58669b737a8bda4460db5699fe47ec9b146
Jorge Olarte <ingeniero@jorgeolarte.com>
2022-04-25 16:14:15 -0500

    Optimizing code

--- pages/api/subscribers/index.js
+++ pages/api/subscribers/index.js
@@ -221,6 +221,7 @@ export default async function handler(req, res) {
   const { fields } = req.body.data;
 
   let subscriber = fields.find((index) => index.key === "question_nW5o2v");
+
   // Validar si quiere suscribirse
   if (subscriber.value === "a48e858b-4e6a-4399-a94d-506a7feac067") {
     // Obtener el correo, nombre y telefono
